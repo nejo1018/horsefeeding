8cade476995c9b4d68913094b3aef0f3e035cb35
Neumaier <jonas.neumaier@accenture.com>
2024-11-23 15:58:54 +0100

    fixed feedingstation

--- src/main/java/com/accenture/javengers/horsefeeding/feedingstation/Feedingstation.java
+++ src/main/java/com/accenture/javengers/horsefeeding/feedingstation/Feedingstation.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalTime;
 
-/*
+
 @Entity
 @Table(name = "feedingstation")
 
@@ -48,58 +48,6 @@ public class Feedingstation {
     }
 
 }
- */
 
-@Entity
-@Table(name = "feedingstation")
-public class Feedingstation {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "feedingstation_id")
-    private Integer id;
-
-    @ManyToOne
-    @JoinColumn(name = "stable_id", nullable = false)
-    private Stable stable;
-
-    @Column(name = "feeding_range_start", nullable = false)
-    private LocalTime feedingRangeStart;
-
-    @Column(name = "feeding_range_end", nullable = false)
-    private LocalTime feedingRangeEnd;
-
-    // Getter und Setter
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Stable getStable() {
-        return stable;
-    }
-
-    public void setStable(Stable stable) {
-        this.stable = stable;
-    }
-
-    public LocalTime getFeedingRangeStart() {
-        return feedingRangeStart;
-    }
-
-    public void setFeedingRangeStart(LocalTime feedingRangeStart) {
-        this.feedingRangeStart = feedingRangeStart;
-    }
-
-    public LocalTime getFeedingRangeEnd() {
-        return feedingRangeEnd;
-    }
-
-    public void setFeedingRangeEnd(LocalTime feedingRangeEnd) {
-        this.feedingRangeEnd = feedingRangeEnd;
-    }
-}
 
--- src/main/java/com/accenture/javengers/horsefeeding/feedingstation/FeedingstationDto.java
+++ src/main/java/com/accenture/javengers/horsefeeding/feedingstation/FeedingstationDto.java
@@ -1,6 +1,5 @@
 package com.accenture.javengers.horsefeeding.feedingstation;
 
-/*
 public class FeedingstationDto {
 
     private Integer feedingstation_id;
@@ -29,39 +28,5 @@ public class FeedingstationDto {
 
     public void setStable_name(String stable_name) {
         this.stable_name = stable_name;
-    }
-}
-
- */
-
-public class FeedingstationDto {
-
-    private Integer feedingstation_id;
-    private String stable_name;
-    private String feeding_range;
-
-    // Getter und Setter
-    public Integer getFeedingstation_id() {
-        return feedingstation_id;
-    }
-
-    public void setFeedingstation_id(Integer feedingstation_id) {
-        this.feedingstation_id = feedingstation_id;
-    }
-
-    public String getStable_name() {
-        return stable_name;
-    }
-
-    public void setStable_name(String stable_name) {
-        this.stable_name = stable_name;
-    }
-
-    public String getFeeding_range() {
-        return feeding_range;
-    }
-
-    public void setFeeding_range(String feeding_range) {
-        this.feeding_range = feeding_range;
     }
 }
